dcd0a7511c659634909ffee5a8c4e4afce694334
mishka251 <mishkabelka251@gmail.com>
2019-12-25 01:34:30 +0500

    Правки reader, переписан main

--- reader.py
+++ reader.py
@@ -1,9 +1,10 @@
-from typing import List
+from typing import List, TextIO
 import numpy as np
 
 
-def read_data():
-    # Data read from W05scEpot.dat or W05scBpot.dat:
+# Data read from W05scEpot.dat or W05scBpot.dat:
+
+class Reader:
     csize: int = 28  # const
     d1_pot: int = 15  # const
     d2_pot: int = 18  # const
@@ -12,9 +13,9 @@ def read_data():
     ls: List[int] = np.zeros(csize)
     ms: List[int] = np.zeros(csize)
 
-    alschfits: List[List[float]] = np.zeros(d2_pot, csize)
-    schfits: List[List[float]] = np.zeros(d1_pot, csize)
-    ex_pot: List[List[float]] = np.zeros(2)
+    alschfits: List[List[float]] = np.zeros((d2_pot, csize), np.float)
+    schfits: List[List[float]] = np.zeros((d1_pot, csize), np.float)
+    ex_pot: List[float] = np.zeros(2, np.float)
 
     maxl_pot: int
     maxm_pot: int
@@ -25,7 +26,7 @@ def read_data():
     d2_scha: int = 7  # const
     d3_scha: int = 68  # const
 
-    allnkm: List[List[List[float]]] = np.zeros(d1_scha, d2_scha, d3_scha)
+    allnkm: List[List[List[float]]] = np.zeros((d1_scha, d2_scha, d3_scha), np.float)
 
     maxk_scha: int
     maxm_scha: int
@@ -41,107 +42,97 @@ def read_data():
     bndyb: List[float] = np.zeros(nb)
     ex_bndy: List[float] = np.zeros(2)
 
-    def read_potential(infile: str):
+    def read_potential(self, infile: str):
         """
-        ! Read ascii data file W05scEpot.dat or W05scBpot.dat, written by
-        !   pro write_potential (write_data.pro)
+         Read ascii data file W05scEpot.dat or W05scBpot.dat, written by
+        pro write_potential (write_data.pro)
         """
-        fname: str
-        # Local:
-        i: int
-        lu: int = 20
 
-        csize_rd: int
-        d1_rd: int
-        d2_rd: int
+        file: TextIO = open(infile)
 
-        file = open(infile)
-
-        fname = file.read()
-        ab: List[int] = [int(file.read()) for i in range(28)]
+        _: str = file.read()
+        self.ab: List[int] = [int(file.read()) for i in range(28)]
 
         csize_rd: int = int(file.read())
         d1_rd: int = int(file.read())
         d2_rd: int = int(file.read())
 
-        assert csize_rd == csize, f"('read_potential: file '{infile}': incompatable csize: 'csize_rd=',{csize_rd},' csize=',{csize})"
+        assert csize_rd == self.csize, \
+            f"('read_potential: file '{infile}': incompatable csize: 'csize_rd=',{csize_rd},' csize=',{self.csize})"
 
-        assert d1_rd == d1_pot, f"(' read_potential: file '{infile}': incompatable d1: 'd1_rd=',{d1_rd},' d1_pot='{d1_pot})"
+        assert d1_rd == self.d1_pot, \
+            f"(' read_potential: file '{infile}': incompatable d1: 'd1_rd=',{d1_rd},' d1_pot='{self.d1_pot})"
 
-        assert d2_rd == d2_pot, f"(' read_potential: file '{infile}': incompatable d2: ','d2_rd='{d2_rd}' d2_pot='{d2_pot})"
+        assert d2_rd == self.d2_pot, \
+            f"(' read_potential: file '{infile}': incompatable d2: ','d2_rd='{d2_rd}' d2_pot='{self.d2_pot})"
 
-        for i in range(1, csize):
+        for i in range(self.csize):
             for j in range(6):
-                alschfits[i][j] = float(file.read())
+                self.alschfits[i][j] = float(file.read())
                 pass
             pass
 
-        ex_pot = [float(file.read()), float(file.read())]
+        self.ex_pot = [float(file.read()), float(file.read())]
 
-        for i in range(28):
-            ls[i] = int(file.read())
+        for i in range(self.csize):
+            self.ls[i] = int(file.read())
 
-        maxl_pot = int(file.read())
-        maxm_pot = int(file.read())
+        self.maxl_pot = int(file.read())
+        self.maxm_pot = int(file.read())
 
-        for i in range(28):
-            ms[i] = int(file.read())
+        for i in range(self.csize):
+            self.ms[i] = int(file.read())
 
-        for i in range(csize):
+        for i in range(self.csize):
             for j in range(6):
-                schfits[i][j] = float(file.read())
+                self.schfits[i][j] = float(file.read())
                 pass
             pass
 
         file.close()
         return
 
-    def read_schatable(infile: str):
-        i: int
-        j: int
-        fname: str
-
+    def read_schatable(self, infile: str):
         file = open(infile)
-        fname = file.read()
+        _: str = file.read()
 
-        maxk_scha = int(file.read())
-        maxm_scha = int(file.read())
+        self.maxk_scha = int(file.read())
+        self.maxm_scha = int(file.read())
 
-        for i in range(d3_scha):
-            for j in range(d2_scha):
+        for i in range(self.d3_scha):
+            for j in range(self.d2_scha):
                 for k in range(6):
-                    allnkm[i][j][k] = float(file.read())
+                    self.allnkm[i][j][k] = float(file.read())
                     pass
                 pass
             pass
 
         for i in range(8):
-            th0s[i] = float(file.read())
+            self.th0s[i] = float(file.read())
 
         return
 
-    def read_bndy(infile:str):
-        rd_na:int
-        rd_nb:int
-
+    def read_bndy(self, infile: str):
         file = open(infile)
 
-        fname:str = file.read()
+        _: str = file.read()
         rd_na = int(file.read())
         rd_nb = int(file.read())
 
-        assert rd_na == na, f"('read_potential: file '{file}': incompatable na: ','rd_na='{rd_na}' na=',{na})"
+        assert rd_na == self.na, \
+            f"('read_potential: file '{file}': incompatable na: ','rd_na='{rd_na}' na=',{self.na})"
 
-        assert rd_nb == nb, f"('>>> read_potential: file '{file}': incompatable nb: ','rd_nb='{rd_nb}' nb={nb})"
-
-        for i in range(8):
-            bndya[i] = float(file.read())
-
-        for i in range(8):
-            bndyb[i] = float(file.read())
+        assert rd_nb == self.nb, \
+            f"('>>> read_potential: file '{file}': incompatable nb: ','rd_nb='{rd_nb}' nb={self.nb})"
 
         for i in range(8):
-            ex_bndy[i] = float(file.read())
+            self.bndya[i] = float(file.read())
+
+        for i in range(8):
+            self.bndyb[i] = float(file.read())
+
+        for i in range(8):
+            self.ex_bndy[i] = float(file.read())
 
         file.close()
-
+        return
